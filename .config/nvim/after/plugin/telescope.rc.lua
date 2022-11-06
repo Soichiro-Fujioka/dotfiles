259659e12593badd7ffe532a69d19fda05b72cd4
Soichiro.Fujioka <soichrof@gmail.com>
2022-11-06 21:50:03 +0900

    feat(nvim) add telescope rc

---
+++ .config/nvim/after/plugin/telescope.rc.lua
@@ -0,0 +1,61 @@
+local status, telescope = pcall(require, "telescope")
+if (not status) then return end
+local actions = require('telescope.actions')
+local builtin = require("telescope.builtin")
+
+local function telescope_buffer_dir()
+  return vim.fn.expand('%:p:h')
+end
+
+local fb_actions = require "telescope".extensions.file_browser.actions
+
+telescope.setup {
+  defaults = {
+    mappings = {
+      n = {
+        ["q"] = actions.close
+      },
+    },
+  },
+  extensions = {
+    file_browser = {
+      theme = "dropdown",
+      hijack_netrw = true,
+      mappings = {
+        ["n"] = {
+          ["N"] = fb_actions.create,
+          ["h"] = fb_actions.goto_parent_dir,
+          ["r"] = fb_actions.rename,
+          ["m"] = fb_actions.move,
+          ["d"] = fb_actions.remove,
+          ["c"] = fb_actions.copy,
+        },
+      },
+    },
+  },
+}
+
+telescope.load_extension("file_browser")
+
+vim.keymap.set('n', ';f',
+  function()
+    builtin.find_files({
+      no_ignore = false,
+      hidden = true
+    })
+  end)
+vim.keymap.set('n', ';r', function()
+  builtin.live_grep()
+end)
+vim.keymap.set("n", "sf", function()
+  telescope.extensions.file_browser.file_browser({
+    path = "%:p:h",
+    cwd = telescope_buffer_dir(),
+    respect_gitignore = false,
+    hidden = true,
+    grouped = true,
+    previewer = false,
+    initial_mode = "normal",
+    layout_config = { height = 40 }
+  })
+end)
