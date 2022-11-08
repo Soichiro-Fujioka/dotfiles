4712f1f20b239a8241c1804c1623ebfda0f34eb0
Soichiro.Fujioka <soichrof@gmail.com>
2022-11-08 16:03:09 +0900

    feat(nvim) remove pylint diagnostics

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -16,7 +16,6 @@ null_ls.setup {
   sources = {
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.formatting.prettier,
-    null_ls.builtins.diagnostics.pylint,
     null_ls.builtins.diagnostics.flake8,
     null_ls.builtins.formatting.black,
   },
