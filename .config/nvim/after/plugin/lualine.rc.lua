12100561a6aab94b2b7759ed0de8569345cc5792
Soichiro.Fujioka <soichrof@gmail.com>
2022-12-28 11:52:08 +0900

    feat(lualine) show file path on bottom line

--- .config/nvim/after/plugin/lualine.rc.lua
+++ .config/nvim/after/plugin/lualine.rc.lua
@@ -19,6 +19,14 @@ lualine.setup {
     lualine_y = {},
     lualine_z = {},
   },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {{'filename', path = 1}},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
 }
 
 local keymap = vim.keymap
