8553ac746b47ee060ceb7503faee41723ba007a7
@636322 <soichiro.fujioka@cognizant.com>
2023-01-02 19:36:20 +0900

    fix(maps) fix invalid d or x map

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -1,11 +1,10 @@
 local keymap = vim.keymap
 
-keymap.set('n', 'x', '"_x')
-keymap.set('n', 'dw', 'vb"_d')
 keymap.set('n', '<C-a>', 'gg<S-v>G')
 
-keymap.set('n', 'd', '_d')
-keymap.set('n', 'D', '_D')
+keymap.set('n', 'x', '"_x')
+keymap.set('n', 'd', '"_d')
+keymap.set('n', 'D', '"_D')
 
 keymap.set('i', 'jj', '<ESC>')
 
