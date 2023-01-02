5ca52af6f635296c8716a8b25a1e8fd6e44fb35b
@636322 <soichiro.fujioka@cognizant.com>
2023-01-02 14:34:48 +0900

    feat(lsp) add some usefull functions

--- .config/nvim/after/plugin/mason.rc.lua
+++ .config/nvim/after/plugin/mason.rc.lua
@@ -7,22 +7,18 @@ if (not status2) then return end
 
 mason.setup({})
 
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local opts = { noremap = true, silent = true }
-
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-end
+-- local on_attach = function(client, bufnr)
+--   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+--   local opts = { noremap = true, silent = true }
+-- end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 mason_lspconfig.setup_handlers {
-  function (server_name) -- default handler (optional)
+  function (server_name)
     lspconfig[server_name].setup {
-      on_attach = on_attach, --keyバインドなどの設定を登録
-      capabilities = capabilities, --cmpを連携
+      -- on_attach = on_attach,
+      capabilities = capabilities,
     }
   end,
 }
