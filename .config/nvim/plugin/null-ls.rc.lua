499e41004650cb03f811500fc717eca4748c3a42
Soichiro.Fujioka <soichrof@gmail.com>
2023-02-25 18:25:14 +0900

    feat(nvim) add eslint config in null_ls

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -15,9 +15,8 @@ end
 null_ls.setup {
   sources = {
     null_ls.builtins.formatting.prettierd,
-    -- null_ls.builtins.diagnostics.eslint.with({
-    --   diagnostics_format = '[eslint] #{m}\n(#{c})'
-    -- }),
+    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.diagnostics.flake8,
     null_ls.builtins.formatting.black,
   },
