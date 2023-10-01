9a3de049ee35c31d833c0a30e53ccdab8ceed8dc
Soichiro.Fujioka <soichrof@gmail.com>
2023-10-02 07:23:53 +0900

    disable conda

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -74,8 +74,10 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f $HOME/anaconda3/bin/conda
-    eval $HOME/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-end
+# if test -f $HOME/anaconda3/bin/conda
+#     eval $HOME/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# end
 # <<< conda initialize <<<
 
+
+set -x HOMEBREW_PREFIX /usr/local
