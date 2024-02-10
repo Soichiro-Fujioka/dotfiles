7240364d02eda09ccaa28bd1984a344ae92733da
Soichiro.Fujioka <soichrof@gmail.com>
2024-02-10 20:58:06 +0900

    feat(lualine) almost default config

--- .config/nvim/after/plugin/lualine.rc.lua
+++ .config/nvim/after/plugin/lualine.rc.lua
@@ -1,27 +1,40 @@
-if (not status) then return end
-local status, lualine = pcall(require, "lualine")
-if (not status) then return end
-
-lualine.setup {
-  tabline = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { require'tabline'.tabline_buffers },
-    lualine_x = { require'tabline'.tabline_tabs },
-    lualine_y = {},
-    lualine_z = {},
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'nordic',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
   },
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {{'filename', path = 1}},
+    lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
-  options = {
-    -- theme = 'neon'
-    -- theme = 'tokyonight'
-    theme = 'nordic'
-  }
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 }
