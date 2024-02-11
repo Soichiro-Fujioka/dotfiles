d530ca900e75876f3f090c740e240ef5a58c3e03
Soichiro.Fujioka <soichrof@gmail.com>
2024-02-11 15:57:02 +0900

    feat(barbar) setup barbar

--- .config/nvim/after/plugin/barbar.rc.lua
+++ .config/nvim/after/plugin/barbar.rc.lua
@@ -1,6 +1,14 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
--- keymap.set('n', 'te', ':TablineTabNew')
 keymap.set('n', '<S-Tab>', ':BufferPrevious<CR>', opts)
 keymap.set('n', '<Tab>', ':BufferNext<CR>', opts)
-keymap.set('n', '<C-w>', ':BufferClose<CR>', opts)
+keymap.set('n', '<C-Tab>', ':BufferClose<CR>', opts)
+
+require'barbar'.setup {
+  clickable = false,
+  icons = {
+    button = '',
+    separator = {left = '', right = ''},
+  },
+  animation = false
+}
