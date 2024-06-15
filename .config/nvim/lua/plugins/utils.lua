8cc950a9c1af38d6a76cef735dbfac6af65d0aa0
Soichiro.Fujioka <soichrof@gmail.com>
2024-06-15 12:03:42 +0900

    feat(floaterm) add floaterm plugin

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -32,4 +32,14 @@ return {
       },
     },
   },
+  {
+    "voldikss/vim-floaterm",
+    keys = {
+      { ";;", "<cmd>FloatermToggle --height=0.8 --width=0.8 <Return>", desc = "Show float term(toggle)" },
+    },
+    config = function()
+      vim.g.floaterm_height = 0.8
+      vim.g.floaterm_width = 0.8
+    end,
+  },
 }
