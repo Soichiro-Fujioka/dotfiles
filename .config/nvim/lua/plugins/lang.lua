384cd36f1215a01d3de9f3fb70ae5bad0f8c438e
Soichiro.Fujioka <soichrof@gmail.com>
2024-07-22 17:00:20 +0900

    feat(markdown) add markdown preview

--- .config/nvim/lua/plugins/lang.lua
+++ .config/nvim/lua/plugins/lang.lua
@@ -9,4 +9,13 @@ return {
     end,
     cmd = { "SwaggerPreview" },
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
