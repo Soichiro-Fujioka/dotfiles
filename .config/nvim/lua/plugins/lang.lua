ae0002d3e7b1e5548155eeca3278d1e54258c44d
Soichiro.Fujioka <soichrof@gmail.com>
2024-08-02 07:10:28 +0900

    feat(markdown-preview) uninstall markdown-preview

--- .config/nvim/lua/plugins/lang.lua
+++ .config/nvim/lua/plugins/lang.lua
@@ -9,13 +9,4 @@ return {
     end,
     cmd = { "SwaggerPreview" },
   },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-  },
 }
