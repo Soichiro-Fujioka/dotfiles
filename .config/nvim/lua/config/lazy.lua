c453303a901410b0424bed0616c85e1d4e0636d7
Soichiro.Fujioka <soichrof@gmail.com>
2024-09-01 21:30:52 +0900

    feat(dial) add dial

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.editor.dial" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,3 +1,14 @@
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/editor/dial.lua
+local M = {}
+function M.dial(increment, g)
+  local mode = vim.fn.mode(true)
+  -- Use visual commands for VISUAL 'v', VISUAL LINE 'V' and VISUAL BLOCK '\22'
+  local is_visual = mode == "v" or mode == "V" or mode == "\22"
+  local func = (increment and "inc" or "dec") .. (g and "_g" or "_") .. (is_visual and "visual" or "normal")
+  local group = vim.g.dials_by_ft[vim.bo.filetype] or "default"
+  return require("dial.map")[func](group)
+end
+
 return {
   {
     "folke/flash.nvim",
@@ -80,6 +91,30 @@ return {
     "folke/trouble.nvim",
     keys = {
       { ";e", "<cmd>Trouble diagnostics toggle<Return>", desc = "Diagnostics (Trouble)" },
+    },
+  },
+  {
+
+    "monaqa/dial.nvim",
+    keys = {
+      {
+        "<C-d>",
+        function()
+          return M.dial(true)
+        end,
+        expr = true,
+        desc = "Increment",
+        mode = { "n", "v" },
+      },
+      {
+        "<C-s>",
+        function()
+          return M.dial(false)
+        end,
+        expr = true,
+        desc = "Decrement",
+        mode = { "n", "v" },
+      },
     },
   },
 }
