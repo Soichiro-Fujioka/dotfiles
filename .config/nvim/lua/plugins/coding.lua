c87ede8bedc9f8dd8ced5cee7b2db654a1b6a08e
Soichiro.Fujioka <soichrof@gmail.com>
2024-09-16 09:23:41 +0900

    feat(im-select) add plugin

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,8 +1,18 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = { "hrsh7th/cmp-emoji" },
-  ---@param opts cmp.ConfigSchema
-  opts = function(_, opts)
-    table.insert(opts.sources, { name = "emoji" })
-  end,
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
+  {
+    "keaising/im-select.nvim",
+    opts = {
+      default_im_select = "com.apple.keylayout.ABC",
+      set_default_events = { "VimEnter", "InsertEnter", "InsertLeave" },
+      set_previous_events = {},
+    },
+  },
 }
