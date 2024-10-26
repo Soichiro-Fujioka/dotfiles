76eab64e8fe915c37880594a58d74d60e7e10f88
Soichiro.Fujioka <soichrof@gmail.com>
2024-10-27 07:19:23 +0900

    feat(dashboard) update dashboard

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -93,6 +93,58 @@ return {
 
       logo = string.rep("\n", 8) .. logo .. "\n\n"
       opts.config.header = vim.split(logo, "\n")
+      opts.config.center = {
+        {
+          action = "lua LazyVim.pick()()",
+          desc = " Find File",
+          icon = " ",
+          key = "f",
+        },
+        {
+          action = "ene | startinsert",
+          desc = " New File",
+          icon = " ",
+          key = "n",
+        },
+        {
+          action = 'lua LazyVim.pick("oldfiles")()',
+          desc = " Recent Files",
+          icon = " ",
+          key = "r",
+        },
+        {
+          action = 'lua LazyVim.pick("live_grep")()',
+          desc = " Find Text",
+          icon = " ",
+          key = "g",
+        },
+        {
+          action = "lua LazyVim.pick.config_files()()",
+          desc = " Config",
+          icon = " ",
+          key = "c",
+        },
+        {
+          action = "LazyExtras",
+          desc = " Lazy Extras",
+          icon = " ",
+          key = "x",
+        },
+        {
+          action = "Lazy",
+          desc = " Lazy",
+          icon = "󰒲 ",
+          key = "l",
+        },
+        {
+          action = function()
+            vim.api.nvim_input("<cmd>qa<cr>")
+          end,
+          desc = " Quit",
+          icon = " ",
+          key = "q",
+        },
+      }
     end,
   },
   {
