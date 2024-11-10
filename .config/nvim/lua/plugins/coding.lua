8c44cffc32beab1820009c8a45e43283aa8ebcd2
Soichiro.Fujioka <soichrof@gmail.com>
2024-11-10 10:13:37 +0900

    feat(skk) update config

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -17,6 +17,22 @@ return {
   },
   {
     "vim-skk/skkeleton",
+    enabled = false,
+    config = function()
+      vim.api.nvim_set_keymap("i", "<C-j>", "<Plug>(skkeleton-enable)", { noremap = true, silent = false })
+      vim.api.nvim_set_keymap("c", "<C-j>", "<Plug>(skkeleton-enable)", { noremap = true, silent = false })
+      vim.api.nvim_set_keymap("i", "<C-l>", "<Plug>(skkeleton-disable)", { noremap = true, silent = false })
+      vim.api.nvim_set_keymap("c", "<C-l>", "<Plug>(skkeleton-disable)", { noremap = true, silent = false })
+      vim.fn["skkeleton#config"]({
+        globalDictionaries = {
+          "~/.config/nvim/skk/skk-jisyo",
+        },
+        eggLikeNewline = true,
+        keepState = true,
+        showCandidatesCount = 3,
+        -- registerConvertResult = true,
+      })
+    end,
     dependencies = { "vim-denops/denops.vim" },
   },
 }
