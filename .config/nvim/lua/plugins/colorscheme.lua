377ceeb43ad3d113af0bf2854f0902ac052742e5
Soichiro.Fujioka <soichrof@gmail.com>
2025-03-08 20:19:14 +0900

    feat(scheme) update color scheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -34,9 +34,25 @@ return {
     end,
   },
   {
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require("kanagawa").setup({
+        transparent = true,
+        theme = "wave",
+        background = {
+          -- dark = "wave",
+          dark = "dragon",
+          light = "lotus",
+        },
+      })
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfox",
+      colorscheme = "kanagawa",
     },
   },
 }
