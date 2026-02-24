49333089647117acf6f87f5e81edc12ff93657ed
Soichiro.Fujioka <soichrof@gmail.com>
2026-02-24 14:53:45 +0900

    feat(fish) update config fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,6 @@
 set fish_greeting ""
 
 set -gx TERM xterm-256color
-
 set -gx XDG_CONFIG_HOME ~/.config
 
 # theme
@@ -34,72 +33,20 @@ alias gph='git push origin'
 alias gpl='git pull origin'
 alias glo='git log --oneline'
 alias gfc='git fetch --all'
-
 alias lg='lazygit'
 
 set -gx EDITOR nvim
-
-set -gx PATH bin $PATH
-set -gx PATH ~/bin $PATH
-set -gx PATH ~/.local/bin $PATH
-
-# delta
 set -gx XDG_CONFIG_HOME "$HOME/.config"
-
-# NodeJS
-set -gx PATH node_modules/.bin $PATH
-# set -x PATH /opt/homebrew/bin/nodebrew/current/bin $PATH
-# set -x NODEBREW_ROOT /opt/homebrew/bin/nodebrew
-
-# Go
-set -x PATH $HOME/go/bin $PATH
-
-# homebrew
-set -x PATH /opt/homebrew/bin $PATH
-set -x PATH /opt/homebrew/sbin $PATH
 
 # docker
 set -x DOCKER_CONFIG $HOME/.docker
-
-# TODO move to local file
-# NVM
-function __check_rvm --on-variable PWD --description 'Do nvm stuff'
-    status --is-command-substitution; and return
-
-    if test -f .nvmrc; and test -r .nvmrc
-        nvm use
-    else
-    end
-end
 
 set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
 if test -f $LOCAL_CONFIG
     source $LOCAL_CONFIG
 end
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-# if test -f $HOME/anaconda3/bin/conda
-#     eval $HOME/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-# end
-# <<< conda initialize <<<
-
-set -x HOMEBREW_PREFIX /usr/local
 set -x TZ Asia/Tokyo
-
-# ASDF configuration code
-# if test -z $ASDF_DATA_DIR
-#     set _asdf_shims "$HOME/.asdf/shims"
-# else
-#     set _asdf_shims "$ASDF_DATA_DIR/shims"
-# end
-#
-# # Do not use fish_add_path (added in Fish 3.2) because it
-# # potentially changes the order of items in PATH
-# if not contains $_asdf_shims $PATH
-#     set -gx --prepend PATH $_asdf_shims
-# end
-# set --erase _asdf_shims
 
 # Auto start/attach tmux on interactive shell
 if status is-interactive
@@ -109,5 +56,3 @@ if status is-interactive
         end
     end
 end
-
-starship init fish | source
