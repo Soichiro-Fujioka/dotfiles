5ea97bdb61dd4333bd6dee307a9cc563b6704224
Soichiro.Fujioka <soichrof@gmail.com>
2026-03-04 17:32:17 +0900

    feat(markdown-preview) add custom css

--- .config/nvim/lua/plugins/lang.lua
+++ .config/nvim/lua/plugins/lang.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_markdown_css = vim.fn.stdpath("config") .. "/markdown-preview.css"
+    end,
+    ft = { "markdown" },
+  },
+  {
     "vinnymeller/swagger-preview.nvim",
     config = function()
       require("swagger-preview").setup({
